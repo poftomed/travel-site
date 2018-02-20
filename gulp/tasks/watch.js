bcffd0893fefbd8cf9d845b78e0bebdc22619a36
Jose Chavez <chavzjosemed@gmail.com>
2018-02-20 00:20:03 -0600

    reveal on scroll

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,7 +17,7 @@ gulp.task('watch', function() {
 
 	watch('./app/assets/styles/**/*.css', function(){
 		gulp.start('cssInject')
-	})
+	});
 
 	watch('./app/assets/scripts/**/*.js', function(){
 		gulp.start('scriptsRefresh'); 
